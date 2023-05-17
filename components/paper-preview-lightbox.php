6d246f961cc89382d389f128be5d7bb8f8ebae56
H1ako <nikita-sobolev-wd@yandex.ru>
2023-05-17 19:56:01 +0400

    added localization for paper preview

--- components/paper-preview-lightbox.php
+++ components/paper-preview-lightbox.php
@@ -1,5 +1,5 @@
-<?php global $SITE_URL; ?>
+<?php global $SITE_URL, $currentLocale; ?>
 <dialog class="preview-lightbox preview-lightbox_paper-preview" id="paper-preview-lightbox">
   <img src="" alt="<?= $localeReader->translate('Signature Preview Image'); ?>" class="paper-preview__image" width="400" height="300">
-  <img src="<?= "$SITE_URL/assets/images/preview-bg.png" ?>" alt="<?= $localeReader->translate('Background Image'); ?>" class="paper-preview__bg-image" width="1200" height="800">
+  <img src="<?= $SITE_URL ?>/assets/images/preview-bg-<?= $currentLocale['short_code'] ?>.png" alt="<?= $localeReader->translate('Background Image'); ?>" class="paper-preview__bg-image" width="1200" height="800">
 </dialog>
